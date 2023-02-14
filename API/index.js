b615ba559a18277d945d5c763ce328eb9db23b85
rafi <r.rafi1615005@gmail.com>
2023-02-14 07:48:48 +0600

    login-completed

--- API/index.js
+++ API/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const auth = require('./routes/auth')
 
 dotenv.config()
 const app = express();
@@ -12,6 +13,8 @@ mongoose.connect(process.env.URL)
                     .then(()=>{console.log("MongoDB is connected")})
                     .catch((err) => {console.log(err)})
 
+
+app.use('/api/auth', auth)
 app.listen(PORT, ()=>{
     console.log('listening on port', PORT);
 })
--- API/routes/auth.js
+++ API/routes/auth.js
@@ -1,15 +1,46 @@
 const router = require('mongoose').Router();
 const User = require('./../models/User');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+
+const generateAccessToken = (user) =>{
+    return jwt.sign({id: user.id, isAdmin: user.isAdmin}, "mySecretKey", {expiresIn: "5s"});
+}
+
+const generateRefreshToken = (user) =>{
+    return jwt.sign({id: user.id, isAdmin: user.isAdmin}, "RefreshToken")
+}
 
 // REGISTER
-router.get('/register', async (req, res) => {
-    try {
-        const newUser = User(req.body);
-        await newUser.save()
-    } catch(err){
-        console.log(err);
-    }
+router.post('/register', async (req, res) => {
+    // await User.insertOne(req.body)
+    console.log(req.body)
 })
 
 // LOGIN
+router.post('/login', async (req, res) => {
+    const authHeader = req.headers.authorization
+    if(authHeader) {
+        const token = authHeader.split(" ")[1];
+        
+        jwt.verify(token, "mySecretKey", (err, data)=>{
+            if(err) {
+                res.status(403).send("Token is not valid1")
+            }
+
+            else{
+                req.user = data;
+            }
+        })
+    } else {
+        res.status(401).json("You are not authorized!!")
+    }
+})
+
+
+// LOGOUT
+
+// DELETE
+
+module.exports = router;
